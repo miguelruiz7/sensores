e70520b8dd02ee33aa0857eb9927ad9ecdf6dbbc
Miguel Ruiz Zamora <58411768+miguelruiz7@users.noreply.github.com>
2023-07-26 00:14:00 -0600

    -------

--- v1.2/backend/info.php
+++ v1.2/backend/info.php
@@ -2,7 +2,7 @@
 // Solo guarda información acerca del desarrollo 
 $titulo = 'CITNOVA';
 $desarrolladores = array('Miguel Ruiz Zamora');
-$organizacion = '?';
+$organizacion = 'Intellivation Technologies';
 $fecha_desarrollo = '26/05/2023';
 $anio = date('Y');
 $version = 'v1.0';
